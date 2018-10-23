435c05dd1cbf22edd2cfd50348051bfd1bf205db
OStillman <owenstillman@live.co.uk>
2018-10-23 12:34:58 +0100

    Auth Response

--- main.py
+++ main.py
@@ -7,6 +7,13 @@ template_env = jinja2.Environment(
 )
 
 class MainPage(webapp2.RequestHandler):
+    def post(self):
+        recieved_data = self.request.get('challenge')
+        template = template_env.get_template('auth.html')
+        context = {
+            'data': recieved_data,
+        }
+        self.response.out.write(template.render(context))
 
 
-application = webapp2.WSGIApplication([('/', MainPage)], debug=True) # Debug will be false when in production
+application = webapp2.WSGIApplication([('/', MainPage)], debug=True)  # Debug will be false when in production
